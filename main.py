2a7d36183e6946fe5aa2de4395b89e2fd543e0f5
Sean Chang <seantc@uci.edu>
2019-11-20 11:02:04 -0800

    Modified to include file configs + added graceful shutdown

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import cv2 as cv
 import argparse
 import time
 import os
+import yaml
+import pprint
+import shutil
+import signal
+import sys
 
-DATA_DIR = "data/"
-os.mkdir(DATA_DIR)
+DATA_DIR = "data"
+pp = pprint.PrettyPrinter(indent=2)
 face_cascade = cv.CascadeClassifier()
 eyes_cascade = cv.CascadeClassifier()
-camera = 0
+camera_port = 0
+camera = None
+
+def signal_handler(sig, frame):
+	print('Gracefully shutting down FIRM ...')
+	if camera != None:
+		camera.release()
+	cv.destroyAllWindows()
+	sys.exit(0)
 
 def getFace(frame):
 	frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -27,8 +40,7 @@ def getFace(frame):
 		if (len(eyes) == 2):
 			ts = str(time.time())
 			print("matched: " + ts)
-			img_name = ts + ".png"
-			cv.imwrite(DATA_DIR + img_name, frame)
+			cv.imwrite(os.path.join(DATA_DIR, ts + ".png"), frame)
 		for (x2,y2,w2,h2) in eyes:
 			# eye_center = (x + x2 + w2//2, y + y2 + h2//2)
 			# radius = int(round((w2 + h2)*0.25))
@@ -36,43 +48,63 @@ def getFace(frame):
 			frame = cv.line(frame, (x + x2, y + y2 + h2//2), (x + x2 + w2, y + y2 + h2//2), (255, 0, 0), thickness=2)
 	return frame
 	
-def loadConfig(fc, ec, c):
+def loadConfig(config_file_name):
 	print("Loading config ...")
-	if not face_cascade.load(cv.samples.findFile(str(fc))):
+	
+	loaded_config = None
+	with open(config_file_name, "r") as config_file:
+		try:
+			loaded_config = yaml.safe_load(config_file) 	
+		except yaml.YAMLError as e:
+			print(e)
+			
+	face_cascade_path = os.path.join("config", loaded_config["face_cascade_file"])
+	eyes_cascade_path = os.path.join("config", loaded_config["eyes_cascade_file"])
+	
+	if not face_cascade.load(cv.samples.findFile(face_cascade_path)):
 		print('ERROR: loading face cascade')
 		exit(0)
-	print("CONFIG: face_cascade = %s" % (fc))
-	if not eyes_cascade.load(cv.samples.findFile(str(ec))):
+	
+	if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_path)):
 		print('ERROR: loading eyes cascade')
 		exit(0)
-	print("CONFIG: eyes_cascade = %s" % (ec))
-	camera = int(c)
-	print("CONFIG: camera = %s" % (c))
+	
+	camera_port = int(loaded_config["camera_port"])
+	
+	pp.pprint(loaded_config)
 	print("Config loading complete.\n")
 
 def start_webcam():
 	print("Starting webcam ...")
-	cam = cv.VideoCapture(0)
+	camera = cv.VideoCapture(camera_port, cv.CAP_DSHOW)
 	while True:
-		ret, frame = cam.read()
+		ret, frame = camera.read()
 		if frame is None:
 			break
 		frame = cv.flip(frame, 1)
 		frame = getFace(frame)
 		cv.imshow('webcam', frame)
-		if cv.waitKey(1) == 27: 
-			break  # esc to quit
-	cv.destroyAllWindows()
+		if cv.waitKey(1) == 27:
+			os.kill(os.getpid(), signal.SIGINT)
 
 def main():
-	parser = argparse.ArgumentParser(description='Entrypoint script for face-identity-registry')
-	parser.add_argument('--face_cascade', help='Path to face cascade.', default='config/haarcascade_frontalface_default.xml')
-	parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='config/haarcascade_eye_tree_eyeglasses.xml')
-	parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+	
+	signal.signal(signal.SIGINT, signal_handler)
+
+	parser = argparse.ArgumentParser(description='Entrypoint script for face-identity-registry-matching')
+	parser.add_argument('-f','--config_file', help='Path to configuration file.', default='config/config.yaml')
+	parser.add_argument('-c','--clean', help='Option to clear data.', action='store_true')
 	args = parser.parse_args()
 	
-	loadConfig(args.face_cascade, args.eyes_cascade, args.camera)
-
+	loadConfig(args.config_file)
+	
+	if args.clean:
+		print("Clearing data ...")
+		shutil.rmtree(DATA_DIR, ignore_errors=True)
+		print("Data directory deleted.\n")
+		
+	os.makedirs(DATA_DIR, exist_ok=True)
+	
 	start_webcam()
 
 if __name__ == "__main__":
