f6976749fa0a320504a11bfae854f59fa394d82c
Sean Chang <seanarwa@gmail.com>
2019-11-24 01:17:47 -0800

    Modified build_windows.bat

--- client/build_windows.bat
+++ client/build_windows.bat
@@ -5,3 +5,4 @@ pip --version
 pip install -r requirements.txt
 pyinstaller main.py
 XCOPY /E config dist\main\config\
+XCOPY /E config\dlib\face_recognition_models dist\main\face_recognition_models\
