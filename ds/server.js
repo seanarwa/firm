47c8609cae691cec284fb27b835764c38041d52e
Sean Chang <seanarwa@gmail.com>
2019-11-25 23:35:04 -0800

    Added firm-ds

---
+++ ds/server.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let dotenv = require('dotenv');
+var multer  = require('multer');
+var fs = require('fs');
+var data = multer({ dest: 'data/' });
+
+// set environment variables
+dotenv.config();
+
+// express app
+const app = express();
+
+// configure app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 226
+
+// Root path
+app.get('/', (req,res) => {
+    return res.end('firm-ds is running');
+});
+
+app.post('/',
+        data.single('image'),
+        (req, res, next) => {
+            console.log(req.body.name)
+            res.send('image received')
+        }
+);
+
+// catch 404
+app.use((req, res, next) => {
+    res.status(404).send('ERROR: request path not found');
+});
+
+// start the server
+var server = app.listen(port,() => {
+    var port = server.address().port;
+    console.log(`firm-ds started on port ${port}`);
+});
+  
+module.exports = app;
